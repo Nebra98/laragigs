f3e173d090864ab06443c5b0de0e50bb02808273
Nebra98 <bulicbrane561@gmail.com>
2023-03-06 19:56:39 +0100

    Filter the index page with tags

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
         return view('listings.index', [
-            'listings' => Listing::all()
+            'listings' => Listing::latest()->filter(\request(['tag']))->get()
         ]);
     }
 
