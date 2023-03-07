43888808bc62f2ffe1473ddb1a73ed3cd49edf52
Nebra98 <bulicbrane561@gmail.com>
2023-03-07 23:41:45 +0100

    Added user_id field to listings table in fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Listing;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,7 +16,14 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        Listing::factory(6)->create();
+        $user = User::factory()->create([
+            'name' => 'Branimir Bulic',
+            'email' => 'bulicbrane561@gmail.com'
+        ]);
+
+        Listing::factory(6)->create([
+            'user_id' => $user->id
+        ]);
 
 //       Listing::create([
 //            'title' => 'Laravel Senior Developer',
